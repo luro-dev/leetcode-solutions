b1b9c9ebb10b241209123fbc0384b5ac1bf2d37e
Luis Rodriguez <106412395+luro-dev@users.noreply.github.com>
2026-02-11 13:21:22 -0500

    File updated at 2/11/2026, 1:21:21 PM

--- 209-MinimumSizeSubarraySum/209-MinimumSizeSubarraySum.py
+++ 209-MinimumSizeSubarraySum/209-MinimumSizeSubarraySum.py
@@ -1,4 +1,4 @@
-# Last updated: 2/11/2026, 1:19:26 PM
+# Last updated: 2/11/2026, 1:21:21 PM
 1"""
 2- Sliding window problem, with a dynamic window
 3- Contraint metric:  subarray sum
@@ -7,23 +7,24 @@
 6- "best" subarray has shortest length
 7- the target can be greater than the entire array sum, in that case return 0
 8
-9- keep left ptr, right ptr to advance until sum > target, fix window with while loop advancing left and removing val at left index, when the window is valid calculate the size and compare to current max min length
-10"""
-11class Solution:
-12    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-13        global_min = float('inf')
-14        cSum = 0
-15        l = 0
-16
-17        for r in range(len(nums)):
-18            cSum += nums[r]
-19
-20            while cSum >= target:
-21                global_min = min(global_min, r - l + 1)
-22                cSum -= nums[l]
-23                l += 1
-24        
-25
-26        return 0 if global_min == float('inf') else global_min
-27
+9- this is similar to problems like subarray sum but with a trick, instead of having a specific end constraint we have a possibility where we need to remove various elements to be less than target, each of those removals shrinks the size of the subarray and we need to also consider their length, so we will have a while loop updating our min value
+10
+11"""
+12class Solution:
+13    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+14        global_min = float('inf')
+15        cSum = 0
+16        l = 0
+17
+18        for r in range(len(nums)):
+19            cSum += nums[r]
+20
+21            while cSum >= target:
+22                global_min = min(global_min, r - l + 1)
+23                cSum -= nums[l]
+24                l += 1
+25        
+26
+27        return 0 if global_min == float('inf') else global_min
 28
+29
