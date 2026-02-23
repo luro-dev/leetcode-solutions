1cbfdf46d44dc3bd75d25298dbb5f3087ea810b7
Luis Rodriguez <106412395+luro-dev@users.noreply.github.com>
2026-02-23 18:17:18 -0500

    File updated at 2/23/2026, 6:17:18 PM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -1,4 +1,4 @@
-# Last updated: 2/23/2026, 6:06:56 PM
+# Last updated: 2/23/2026, 6:17:18 PM
 1"""
 2- So this has to be a counting problem with a sliding window. 
 3- We have to go through the string and keep a map of seen characters, if at any point a new character reaches a count > 1 we must iterate from the left and delete as we go.
@@ -12,11 +12,11 @@
 11        left = 0
 12
 13        for right in range(len(s)):
-14            seen[s[right]] += 1
-15
-16            while seen[s[right]] > 1:
-17                seen[s[left]] -= 1
-18                left += 1
+14
+15            if s[right] in seen:
+16                left = max(left, seen[s[right]] + 1)
+17            
+18            seen[s[right]] = right
 19
 20            max_sub = max(max_sub, right - left + 1)
 21        
