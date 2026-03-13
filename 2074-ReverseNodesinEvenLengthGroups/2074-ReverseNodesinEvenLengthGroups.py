34dd79972650e8330ad92ce18ce232165fa90458
Luis Rodriguez <106412395+luro-dev@users.noreply.github.com>
2026-03-13 12:07:06 -0400

    File updated at 3/13/2026, 12:07:06 PM

--- 2074-ReverseNodesinEvenLengthGroups/2074-ReverseNodesinEvenLengthGroups.py
+++ 2074-ReverseNodesinEvenLengthGroups/2074-ReverseNodesinEvenLengthGroups.py
@@ -1,4 +1,4 @@
-# Last updated: 3/12/2026, 1:32:12 PM
+# Last updated: 3/13/2026, 12:07:06 PM
 1# Definition for singly-linked list.
 2# class ListNode:
 3#     def __init__(self, val=0, next=None):
@@ -6,66 +6,57 @@
 5#         self.next = next
 6
 7"""
-8- Given the head of a linked list. 
-9- The nodes of the list are assigned to groups, who have lengths of the sequence of natural numbers (1st group 1 node , 2nd group 2 nodes, 3rd group 3 nodes, etc.)
-10- We want to reverse the nodes of the groups with even lengths
-11- Remember the edge case for the last group, must check its length it may be less than or equal to the length of the 2nd to last group 
-12
-13
-14- Use a counter variable starting at 1 and increment it every move, keep a pointer to the head of the previous group, this will connect to the newHead of new group, and also track previousHead since it will be the tail that will connect to next group
-15
-16IMPORTANT:
-17- oldGroupTail
-18- newGroupHead
-19- currentGroupHead
-20
-21if reversed curr group, currGroupHead becomes its tail and a prev Value becomes new head so:
-22    oldGroupTail.next = currGroupPrev
-23    currentGroupHead.next = newGroupHead
-24
-25
-26"""
-27class Solution:
-28    # reverse group and return the new head
-29    def reverseGroup(self, head):
-30        prev = None
-31        while head:
-32            next_node = head.next
-33            head.next = prev
-34            prev = head
-35            head = next_node
-36        return prev
-37    def reverseEvenLengthGroups(self, head: Optional[ListNode]) -> Optional[ListNode]:
-38        group_num = 2
-39        curr = head
-40        while curr.next:
-41            group_count = 1
-42            temp = curr.next
-43
-44            while group_count < group_num and temp.next:
-45                group_count += 1
-46                temp = temp.next
-47
-48            if group_count % 2 == 0:
-49                groupStart = curr.next
-50                nextGroupHead = temp.next 
-51
-52                groupEnd = temp
-53                groupEnd.next = None
-54
-55                newHead = self.reverseGroup(groupStart)
-56                curr.next = newHead
-57                groupStart.next = nextGroupHead
-58
-59                curr = groupStart
-60            else:
-61                curr = temp
-62
-63            group_num += 1
-64
-65        return head
-66            
-67
-68
-69        
-70        
+8- We want to reverse only the groups that have an even length, 
+9- The groups will be in order sequentially forming the sequence of natural nums
+10- 1st group 1 node, 2nd 2 nodes, last group can be variable so we have to check len
+11
+12- keep a temp to iterate through the group 
+13- curr will always point to prev tail
+14- iterate temp until group_count is 1 less than group_num to get the curr group tail
+15- always save next groupHead, and set the current groupTail.next to null to establish end point for reversal
+16- if the group count is odd just make sure that curr = temp since we just want to shift curr to the current tail
+17- if the group is even reverse it, the oldhead will be the new tail so connect that to the nextGroupHead and the old tail will now be the new head so make sure to connect curr.next to oldTail
+18- always increment the group_num by one at the end
+19"""
+20class Solution:
+21    def reverseHelper(self, head):
+22        prev = None
+23        while head:
+24            next_node = head.next
+25            head.next = prev
+26            prev = head
+27            head = next_node
+28        
+29        return prev
+30
+31    def reverseEvenLengthGroups(self, head: Optional[ListNode]) -> Optional[ListNode]:
+32        dummy = ListNode(-999, head)
+33        curr = head
+34        group_num = 2
+35
+36        while curr.next:
+37            temp = curr
+38            group_count = 0
+39
+40            while temp.next and group_count < group_num:
+41                temp = temp.next
+42                group_count += 1
+43            
+44            if group_count % 2 == 0:
+45                next_group_head = temp.next
+46                
+47                group_head = curr.next
+48                group_tail = temp
+49                group_tail.next = None
+50
+51                newHead = self.reverseHelper(group_head)
+52
+53                curr.next = newHead
+54                group_head.next = next_group_head
+55                curr = group_head
+56            else:
+57                curr = temp
+58            
+59            group_num += 1
+60
+61        return dummy.next
