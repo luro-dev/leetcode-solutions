ca3bc4a23e29d0c580abd96aea9a683f5a8072d9
Luis Rodriguez <106412395+luro-dev@users.noreply.github.com>
2026-03-13 13:37:50 -0400

    File updated at 3/13/2026, 1:37:49 PM

--- 1290-ConvertBinaryNumberinaLinkedListtoInteger/1290-ConvertBinaryNumberinaLinkedListtoInteger.py
+++ 1290-ConvertBinaryNumberinaLinkedListtoInteger/1290-ConvertBinaryNumberinaLinkedListtoInteger.py
@@ -1,4 +1,4 @@
-# Last updated: 3/13/2026, 1:24:10 PM
+# Last updated: 3/13/2026, 1:37:49 PM
 1# Definition for singly-linked list.
 2# class ListNode:
 3#     def __init__(self, val=0, next=None):
@@ -13,18 +13,10 @@
 12"""
 13class Solution:
 14    def getDecimalValue(self, head: Optional[ListNode]) -> int:
-15        prev = None
-16        while head:
-17            n = head.next
-18            head.next = prev
-19            prev = head
-20            head = n
-21        
-22        number = 0
-23        deg = 0
-24        while prev:
-25            number += (2 ** deg) * prev.val
-26            deg += 1
-27            prev = prev.next
-28
-29        return number
+15        num = 0
+16
+17        while head:
+18            num = (2 * num) + head.val
+19            head = head.next
+20        
+21        return num
