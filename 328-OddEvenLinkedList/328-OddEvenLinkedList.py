b40745a8a76ba318b138f3f7e49277a359caca14
Luis Rodriguez <106412395+luro-dev@users.noreply.github.com>
2026-03-14 11:31:24 -0400

    File updated at 3/14/2026, 11:31:24 AM

--- 328-OddEvenLinkedList/328-OddEvenLinkedList.py
+++ 328-OddEvenLinkedList/328-OddEvenLinkedList.py
@@ -1,33 +1,27 @@
-# Last updated: 3/14/2026, 11:29:30 AM
+# Last updated: 3/14/2026, 11:31:24 AM
 1# Definition for singly-linked list.
 2# class ListNode:
 3#     def __init__(self, val=0, next=None):
 4#         self.val = val
 5#         self.next = next
 6
-7"""
-8- Given a list like 1 -> 2 -> 3 -> 4 -> 5
-9- Group all odd index nodes with each other then all even nodes, maintain order
-10- Use counter starting at 1 and check odd / even with mod
-11
-12- Two pointers, one for odd lst one for even, connecting .next.next 
-13"""
-14class Solution:
-15    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-16        if not head: return head
-17
-18        evenHead = head.next
-19
-20        oddTemp = head
-21        evenTemp = evenHead
-22
-23        while oddTemp and oddTemp.next and evenTemp and evenTemp.next:
-24            oddTemp.next = evenTemp.next
-25            oddTemp = oddTemp.next
-26            evenTemp.next = oddTemp.next
-27            evenTemp = evenTemp.next
-28            
-29        oddTemp.next = evenHead
-30
-31        return head
-32       
+7class Solution:
+8    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+9        if not head: return head
+10
+11        evenHead = head.next
+12
+13        oddTemp = head
+14        evenTemp = evenHead
+15
+16        while evenTemp and evenTemp.next:
+17            oddTemp.next = evenTemp.next
+18            oddTemp = oddTemp.next
+19            
+20            evenTemp.next = oddTemp.next
+21            evenTemp = evenTemp.next
+22            
+23        oddTemp.next = evenHead
+24
+25        return head
+26       
